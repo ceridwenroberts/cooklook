5a3c8327ea2430ce657aca5f11993242d774c262
ceridwenroberts <ceridwen.roberts@gmail.com>
2025-04-09 19:06:00 +0200

    remove breakpiont guide

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,6 @@ type HeaderProps = { className?: string }
 const Header: FC<HeaderProps> = ({ className }) => {
 	return (
 		<>
-			<div className='fixed bg-amber-500 sm:bg-green-500 md:bg-blue-500 lg:bg-pink-500 xl:bg-red-500 m-auto p-3 border'></div>
 			<div className={`flex justify-center py-7 text-sky-700 ${className}`}>
 				<h1 className={`font-princess-sofia text-4xl`}> My Cooklook</h1>
 			</div>
