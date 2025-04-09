9098ce5b4302d9843c187a9c01b1935a90d8d3f5
ceridwenroberts <ceridwen.roberts@gmail.com>
2025-04-09 17:03:31 +0200

    add style tweaks and Pretty fromat

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,28 +1,25 @@
-"use client";
+'use client'
 
-import CategoryPreviewCard from "@/components/CategoryPreviewCard";
-// import StarToggle from "@/components/StarToggle";
-import { useFavoritesContext } from "@/utils/contexts";
-import { FavoritesContextType } from "@/utils/types";
-// import Link from "next/link";
+import CategoryPreviewCard from '@/components/CategoryPreviewCard'
+import { useFavoritesContext } from '@/utils/contexts'
+import { FavoritesContextType } from '@/utils/types'
 
 const Category = () => {
-  const { categories } = useFavoritesContext() as FavoritesContextType;
-  return (
-    <>
-      {/* <h1 className="font-princess-sofia text-sky-700">All Categories</h1> */}
-      <ul className="flex flex-wrap place-content-evenly self-center gap-[1vw] sm:gap-1 md:gap-5 sm:m-5 md:mx-4 md:mb-7 max-w-7xl text-sky-700">
-        {categories && categories.length > 0 ?
-          categories.map((category) => (
-            <CategoryPreviewCard
-              key={category.idCategory}
-              category={category}
-            />
-          ))
-        : <p>Loading categories...</p>}
-      </ul>
-    </>
-  );
-};
+	const { categories } = useFavoritesContext() as FavoritesContextType
+	return (
+		<>
+			<ul className='flex flex-wrap place-content-evenly self-center gap-[1vw] sm:gap-1 md:gap-5 sm:m-5 md:mx-4 md:mb-7 max-w-7xl text-sky-700'>
+				{categories && categories.length > 0 ?
+					categories.map((category) => (
+						<CategoryPreviewCard
+							key={category.idCategory}
+							category={category}
+						/>
+					))
+				:	<p>Loading categories...</p>}
+			</ul>
+		</>
+	)
+}
 
-export default Category;
+export default Category
